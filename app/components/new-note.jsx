ed57166f5e701ae7cf9e6b5c634674148463e015
umerkang66 <umerkang66@gmail.com>
2022-11-15 13:20:36 +0500

    update app

--- app/components/new-note.jsx
+++ app/components/new-note.jsx
@@ -2,21 +2,19 @@ import {
   Form,
   useTransition as useNavigation,
   useActionData,
-} from "@remix-run/react";
-import styles from "./new-note.css";
+} from '@remix-run/react';
+import styles from './new-note.css';
 
 export default function NewNote() {
   // technically, we don't have to add the action prop, because this NewNote is actually being used in notes file in app folder
-
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
-
+  const isSubmitting = navigation.state === 'submitting';
   // These data hooks, can also be called in other components, (other than page components)
   const errData = useActionData();
 
   return (
     <Form method="post" action="/notes" id="note-form">
-      {errData && <p style={{ color: "#cc0000" }}>{errData.message}</p>}
+      {errData && <p style={{ color: '#cc0000' }}>{errData.message}</p>}
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
@@ -27,7 +25,7 @@ export default function NewNote() {
       </p>
       <div className="form-actions">
         <button disabled={isSubmitting}>
-          {isSubmitting ? "Adding..." : "Add Note"}
+          {isSubmitting ? 'Adding...' : 'Add Note'}
         </button>
       </div>
     </Form>
@@ -35,5 +33,5 @@ export default function NewNote() {
 }
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: 'stylesheet', href: styles }];
 }
