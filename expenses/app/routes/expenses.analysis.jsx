6995ae28a32e15b4cee9c0ea64321cc3f3580bf1
umerkang66 <umerkang66@gmail.com>
2022-12-15 10:55:05 +0500

    add expenses app

---
+++ expenses/app/routes/expenses.analysis.jsx
@@ -0,0 +1,3 @@
+export default function ExpenseAnalysisPage() {
+  return <div>ExpenseAnalysis Page</div>;
+}
---
+++ expenses/app/routes/expenses.raw.jsx
@@ -0,0 +1,3 @@
+export default function RawExpensePage() {
+  return <div>RawExpense Page</div>;
+}
