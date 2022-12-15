ec68e4cd87c8cf8ae1b2149ca5d734e22466c342
umerkang66 <umerkang66@gmail.com>
2022-12-15 11:54:26 +0500

    add components and styles

--- expenses/app/routes/expenses.jsx
+++ expenses/app/routes/expenses.jsx
@@ -1,3 +1,11 @@
-export default function ExpensesPage() {
-  return <div>Expenses Page</div>;
+// If there is also a fine with the same name as folder, this acts as layout file for that folder route
+import { Outlet } from '@remix-run/react';
+
+export default function ExpensesLayout() {
+  return (
+    <main>
+      <p>Shared Layout</p>
+      <Outlet />
+    </main>
+  );
 }
